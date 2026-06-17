3793beaccba526b8a0b0f30ce2b823db5bd1b029
octaviolib <octalib1@gmail.com>
2026-06-17 15:58:54 -0300

    Update validaciones.py

--- validaciones.py
+++ validaciones.py
@@ -1,1 +1,31 @@
+#es la validacion que el usuario ingrese un numero decimal
 
+def pedir_float(mensaje):
+
+    while True:
+
+        dato = input(mensaje)
+
+        if dato.upper() == "VOLVER":
+            return None
+#convierte el  dato que se ingresa  a float
+        try:
+            return float(dato)
+#por si lo que se ingresa no es un numero
+        except ValueError:
+            print("Debe ingresar un número válido.")
+            
+def pedir_entero(mensaje):
+
+    while True:
+
+        dato = input(mensaje)
+
+        if dato.upper() == "VOLVER":
+            return None
+
+        try:
+            return int(dato)
+
+        except ValueError:
+            print("Debe ingresar un número entero válido.")
